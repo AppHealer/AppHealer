c9e0582f0a92b5643aeedfd829f11455f79b0b54
spaceAngel <spaceangel@cyberdeck.cz>
2025-03-05 00:20:25 +0100

    Fix: most down widget when no checks total division by zero

--- resources/views/dashboard/monitors/failed.blade.php
+++ resources/views/dashboard/monitors/failed.blade.php
@@ -1,18 +1,20 @@
 <table class="table">
 	<tbody>
 	@foreach($monitors as $monitor)
-		<tr>
-			<td style="width:180px">
-				<a href="{{route('monitors.detail', ['monitor' => $monitor])}}">{{$monitor->name}}</a>
-			</td>
-			<td class="graph align-middle" title="Uptime {{round($monitor->checks_ok * 100 /$monitor->checks_total)}}%">
-				<div class="full">
-					<div class="ok" style="width:{{round($monitor->checks_ok * 100 /$monitor->checks_total)}}%">
+		@if ($monitor->checks_total > 0)
+			<tr>
+				<td style="width:180px">
+					<a href="{{route('monitors.detail', ['monitor' => $monitor])}}">{{$monitor->name}}</a>
+				</td>
+				<td class="graph align-middle" title="Uptime {{round($monitor->checks_ok * 100 /$monitor->checks_total)}}%">
+					<div class="full">
+						<div class="ok" style="width:{{round($monitor->checks_ok * 100 /$monitor->checks_total)}}%">
 
+						</div>
 					</div>
-				</div>
-			</td>
-		</tr>
+				</td>
+			</tr>
+		@endif
 	@endforeach
 	</tbody>
 </table>
