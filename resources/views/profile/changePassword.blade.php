c044dc817d017881721d62ef65ee4647fe732838
spaceAngel <spaceangel@cyberdeck.cz>
2025-03-05 19:22:19 +0100

    Fix: missing template for change password

--- resources/views/profile/changePassword.blade.php
+++ resources/views/profile/changePassword.blade.php
@@ -1,12 +1,58 @@
-@extends('_layouts.mail')
+@extends(
+	'_layouts.app',
+	[
+		'page' => 'profile',
+		'title' =>  __('Change password')
+	]
+)
 @section('content')
-	<div style="font-weight:bold;margin-bottom:15px">
-		{{__('We receive sending password reset request.')}}
-	</div>
-	<div style="padding-bottom:10px">
-		{{__('To set a new password, click on following link or copy it and open in a browser:')}} <br/>
-	</div>
-	<a style="font-weight:bold;color:#4a5568" href="{{route('passwordreset.reset', ['resettoken' => $resettoken])}}">
-		{{route('passwordreset.reset', ['resettoken' => $resettoken])}}
-	</a>
+	<form method="post" action="{{route('profile.changePassword.submit')}}">
+		@csrf
+		<fieldset class="border mb-3 p-3">
+			<div class="row mb-2">
+				<div class="col-md-5 col-sm-12">
+					<label class="col-form-label" for="fieldCurrentPassword">{{ __('Current password') }}</label>
+				</div>
+				<div class="col-md-7 col-sm-12">
+					<input type="password" name="currentPassword" id="fieldCurrentPassword" class="form-control {{ $errors->has('currentPassword') ? 'is-invalid': '' }}" >
+					@if ($errors->hasAny('currentPassword'))
+						<div class="invalid-feedback">
+							{{  $errors->first('currentPassword') }}
+						</div>
+					@endif
+				</div>
+			</div>
+
+			<div class="row mb-2">
+				<div class="col-md-5 col-sm-12">
+					<label class="col-form-label" for="fieldNewPassword">{{ __('New password') }}</label>
+				</div>
+				<div class="col-md-7 col-sm-12">
+					<input type="password" name="newPassword" id="fieldNewPassword" class="form-control {{ $errors->has('newPassword') ? 'is-invalid': '' }}" >
+					@if ($errors->hasAny('newPassword'))
+						<div class="invalid-feedback">
+							{{  $errors->first('newPassword') }}
+						</div>
+					@endif
+				</div>
+			</div>
+
+			<div class="row mb-2">
+				<div class="col-md-5 col-sm-12">
+					<label class="col-form-label" for="fieldPasswordConfirmation">{{ __('Password confirmation') }}</label>
+				</div>
+				<div class="col-md-7 col-sm-12">
+					<input type="password" name="newPassword_confirmation" id="fieldPasswordConfirmation" class="form-control {{ $errors->has('newPassword_confirmation') ? 'is-invalid': '' }}" >
+					@if ($errors->hasAny('newPassword_confirmation'))
+						<div class="invalid-feedback">
+							{{  $errors->first('newPassword_confirmation') }}
+						</div>
+					@endif
+				</div>
+			</div>
+
+		</fieldset>
+		<a href="{{route('profile.view')}}" class="btn btnBack">{{__('Back')}}</a>
+		<input class="btn" value="{{__('Save')}}" type="submit"/>
+	</form>
 @endsection
