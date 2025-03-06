c58bdb474f7178ee1871aa37e8833c997273387f
spaceAngel <spaceangel@cyberdeck.cz>
2025-03-06 14:07:55 +0100

    Fix: mobile  menu toggler event listener add only when element exists

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,14 @@ import './monitorsFitler';
 import './portlet'
 import './graphs'
 
+
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import '../../node_modules/chart.js/dist/chart.umd.js'
 
 window.addEventListener('load', function() {
-    document.getElementById("menu-toggler").addEventListener("click", function () {
-        document.getElementById("menu-currentUser").classList.toggle("d-none");
-    });
+	if (document.getElementById('menu-toggler')) {
+		document.getElementById("menu-toggler").addEventListener("click", function () {
+			document.getElementById("menu-currentUser").classList.toggle("d-none");
+		});
+	}
 })
