a7466946f434360e14d6f6385ca99287e062f1cc
spaceAngel <spaceangel@cyberdeck.cz>
2025-03-21 14:44:53 +0100

    Fix dashboard cards view  on medium and small displays

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,19 @@
 @extends('_layouts.app', ['page' => 'dashboard', 'title' => 'Dashboard'])
 @section('content')
 <div class="dashboard">
-	<div class="row mb-2">
-		<div class="col-md-3 col-sm-12">
+	<div class="row mb-3">
+		<div class="col-xl-3 col-md-6 col-sm-12 mb-3 mb-md-0">
 			@include('dashboard.cards.monitorsSlowest')
 		</div>
 
-		<div class="col-md-3 col-sm-12">
+		<div class="col-xl-3 col-md-6 col-sm-12">
 			@include('dashboard.cards.monitorsFailed')
 		</div>
 		<div class="col"></div>
 	</div>
 
 	<div class="row">
-		<div class="col-md-3 col-sm-12">
+		<div class="col-xl-3 col-md-6 col-sm-12">
 
 			<div class="card">
 				<div class="card-title"><span class="fa fa-user-shield"></span>{{ __('Last Logins') }}</div>
