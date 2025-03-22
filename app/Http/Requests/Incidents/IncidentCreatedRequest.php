6fbbff382d18be5646a92763230dd4a8faff0128
spaceAngel <spaceangel@cyberdeck.cz>
2025-03-22 16:52:36 +0100

    Feature: validation on privileges when creating incident on monitor

--- app/Http/Requests/Incidents/IncidentCreatedRequest.php
+++ app/Http/Requests/Incidents/IncidentCreatedRequest.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace AppHealer\Http\Requests\Incidents;
 
+use AppHealer\Enums\GlobalPrivilegesAction;
+use AppHealer\Enums\GlobalPrivilegesGroup;
+use AppHealer\Models\Monitor;
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -20,6 +24,20 @@ class IncidentCreatedRequest extends FormRequest
 				'exists:monitors,id',
 				'integer',
 				Rule::requiredIf(request('monitor') === null),
+				//phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+				function (string $attribute, mixed $value, Closure $fail): void {
+					if (
+						!auth()->user()->admin
+						&& !auth()->user()->hasGlobalPrivilege(
+							GlobalPrivilegesGroup::MONITORS,
+							GlobalPrivilegesAction::INCIDENT_CREATE
+						)
+						&& !auth()->user()->getRoleInMonitor(
+							Monitor::find($value)
+						)?->canCreateIncident()
+					)
+					$fail("You haven't enough privileges to create incident.");
+				},
 			],
 		];
 	}
